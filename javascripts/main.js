f79c879b64d2ade35cf3e8528f35046e9e6c6fdb
unknown <yaramodesu@gmail.com>
2021-03-10 10:43:29 +0200

    alkais oleen valmis, vähän siivousta pitäis tehdä jos ois todellinen

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,25 @@
 
 
+  w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
 // Wrap every letter in a span
 const textWrapper = document.querySelector('.ml12');
 const textWrapperTwo = document.querySelector('.ml13');
+const textWrapperThree = document.querySelector('.ml14');
+const textWrapperFour = document.querySelector('.ml15');
+
+
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 textWrapperTwo.innerHTML = textWrapperTwo.textContent.replace(/\S/g, "<span class='letterTwo'>$&</span>");
+textWrapperThree.innerHTML = textWrapperThree.textContent.replace(/\S/g, "<span class='letterThree'>$&</span>");
+textWrapperFour.innerHTML = textWrapperFour.textContent.replace(/\S/g, "<span class='letterFour'>$&</span>");
+
+
 const nappiTanne = document.querySelector('.nappi')
+
+
+
+
+if (w >= 1000) {
 
  anime.timeline({loop: false})
   .add({
@@ -14,8 +28,8 @@ const nappiTanne = document.querySelector('.nappi')
     translateZ: 0,
     opacity: [0,1],
     easing: "easeOutExpo",
-    duration: 1200,
-    delay: (el, i) => 1500 + 30 * i
+    duration: 800,
+    delay: (el, i) => 100 + 30 * i
   })
   .add({
       targets: '.ml13 .letterTwo',
@@ -23,13 +37,50 @@ const nappiTanne = document.querySelector('.nappi')
       translateZ: 0,
       opacity: [0,1],
       easing: "easeOutExpo",
-      duration: 1200,
-      delay: (el, i) => 300 + 30 * i})
-    .add({
+      duration: 500,
+      delay: (el, i) => 100 + 30 * i})
+   
+      .add({
       targets: '.nappi',
+      scale: [0,1],
+      duration: 500
+      
+    })  ;
+  } 
+  else {
+
+  anime.timeline({loop: false})
+  .add({
+    targets: '.ml14 .letterThree',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 500,
+    delay: (el, i) => 100 + 30 * i
+  })
+  .add({
+      targets: '.ml15 .letterFour',
+      translateX: [40,0],
+      translateZ: 0,
       opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 500,
+      delay: (el, i) => 100 + 30 * i})
+      .add({
+        targets: '.ml13 .letterTwo',
+        translateX: [40,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 500,
+        delay: (el, i) => 500 + 30 * i}) 
+      .add({
+      targets: '.nappi',
+      scale: [0,1],
+      duration: 800
       
     })  ;
       
   
-
+  }
